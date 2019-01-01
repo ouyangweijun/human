9260bc55c2cc01222ad1de26e8612afc6b76845f
win10_oulen <henry_oulen@163.com>
2019-01-01 20:03:04 +0800

    斐波那契数列

---
+++ h7Coroutine/03-斐波那契数列.py
@@ -0,0 +1,38 @@
+class Feibonaqie(object):
+    def __init__(self,all_num):
+        self.all_num=all_num
+        self.current_num=0
+        self.a=0
+        self.b=1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.current_num < self.all_num:
+            ret = self.a
+
+            self.a,self.b=self.b,self.a+self.b
+            self.current_num +=1
+            return ret
+        else:
+            raise StopIteration
+
+feibo = Feibonaqie(10)
+
+for num in feibo:
+    print(num)
+
+# 运行结果
+#
+# C:\Python372\python.exe D:/human/h7Coroutine/03-斐波那契数列.py
+# 0
+# 1
+# 1
+# 2
+# 3
+# 5
+# 8
+# 13
+# 21
+# 34
+#
+# Process finished with exit code 0

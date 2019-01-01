1bccf316177d7c09ccc4ecae23284531cefb61d7
win10_oulen <henry_oulen@163.com>
2019-01-01 22:43:22 +0800

    greenlet完成多任务

---
+++ h7Coroutine/07-greenlet完成多任务.py
@@ -0,0 +1,29 @@
+import time
+from greenlet import greenlet
+
+
+# gr1 = greenlet(task_1)
+# gr2 = greenlet(task_2)
+#
+# #切换到gr1中执行
+# gr1.switch()
+
+def main():
+    def task_1():
+
+        while True:
+            print("----1----")
+            gr2.switch()
+            time.sleep(0.1)
+
+    def task_2():
+        while True:
+            print("----2----")
+            gr1.switch()
+            time.sleep(0.1)
+
+    gr1 = greenlet(task_1)
+    gr2 = greenlet(task_2)
+    gr1.switch()
+if __name__ == '__main__':
+    main()

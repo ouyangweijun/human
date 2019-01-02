af1c299deb5bb8386efad3e9fff61414f7654886
win10_oulen <henry_oulen@163.com>
2019-01-02 19:24:51 +0800

    协程实现下载器

--- h7Coroutine/11-协程实现下载器.py
+++ h7Coroutine/11-协程实现下载器.py
@@ -17,7 +17,6 @@ def main():
         gevent.spawn(downloader,'1.jpg','https://res.vmallres.com/pimages/detailImg/2018/12/11/201812112233025695075.png'),
         gevent.spawn(downloader, '2.jpg','https://res.vmallres.com/pimages/detailImg/2018/12/11/201812112233023998633.png' ),
         gevent.spawn(downloader, '3.jpg','https://res.vmallres.com/pimages/detailImg/2018/12/11/201812112233033209844.png' ),
-
     ])
 if __name__ == '__main__':
     main()

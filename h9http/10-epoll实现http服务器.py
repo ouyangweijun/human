86089c85fb1c3c1ba27177053f7a683eff75fdbe
win10_oulen <henry_oulen@163.com>
2019-01-03 08:15:49 +0800

    10-epoll实现http服务器

--- h9http/10-epoll实现http服务器.py
+++ h9http/10-epoll实现http服务器.py
@@ -4,108 +4,146 @@
 # @Author   : henry_oulen
 # @Software : PyCharm
 # @File     : 10-epoll实现http服务器.py
-# @Desc     :
+# @Desc     : 10-epoll实现http服务器
 
 import socket
+import time
+import sys
 import re
 import select
 
 
-def service_client(new_socket, request):
-    """为这个客户端返回数据"""
+class WSGIServer(object):
+    """定义一个WSGI服务器的类"""
 
-    # 1. 接收浏览器发送过来的请求 ，即http请求
-    # GET / HTTP/1.1
-    # .....
-    # request = new_socket.recv(1024).decode("utf-8")
-    # print(">>>"*50)
-    # print(request)
+    def __init__(self, port, documents_root):
 
-    request_lines = request.splitlines()
-    print("")
-    print(">" * 20)
-    print(request_lines)
+        # 1. 创建套接字
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 2. 绑定本地信息
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind(("", port))
+        # 3. 变为监听套接字
+        self.server_socket.listen(128)
 
-    # GET /index.html HTTP/1.1
-    # get post put del
-    file_name = ""
-    ret = re.match(r"[^/]+(/[^ ]*)", request_lines[0])
-    if ret:
-        file_name = ret.group(1)
-        # print("*"*50, file_name)
-        if file_name == "/":
-            file_name = "/index.html"
+        self.documents_root = documents_root
 
-    # 2. 返回http格式的数据，给浏览器
+        # 创建epoll对象
+        self.epoll = select.epoll()
+        # 将tcp服务器套接字加入到epoll中进行监听
+        self.epoll.register(self.server_socket.fileno(), select.EPOLLIN|select.EPOLLET)
 
-    try:
-        f = open("./html" + file_name, "rb")
-    except:
-        response = "HTTP/1.1 404 NOT FOUND\r\n"
-        response += "\r\n"
-        response += "------file not found-----"
-        new_socket.send(response.encode("utf-8"))
-    else:
-        html_content = f.read()
-        f.close()
+        # 创建添加的fd对应的套接字
+        self.fd_socket = dict()
 
-        response_body = html_content
+    def run_forever(self):
+        """运行服务器"""
 
-        response_header = "HTTP/1.1 200 OK\r\n"
-        response_header += "Content-Length:%d\r\n" % len(response_body)
-        response_header += "\r\n"
+        # 等待对方链接
+        while True:
+            # epoll 进行 fd 扫描的地方 -- 未指定超时时间则为阻塞等待
+            epoll_list = self.epoll.poll()
 
-        response = response_header.encode("utf-8") + response_body
+            # 对事件进行判断
+            for fd, event in epoll_list:
+                # 如果是服务器套接字可以收数据，那么意味着可以进行accept
+                if fd == self.server_socket.fileno():
+                    new_socket, new_addr = self.server_socket.accept()
+                    # 向 epoll 中注册 连接 socket 的 可读 事件
+                    self.epoll.register(new_socket.fileno(), select.EPOLLIN | select.EPOLLET)
+                    # 记录这个信息
+                    self.fd_socket[new_socket.fileno()] = new_socket
+                # 接收到数据
+                elif event == select.EPOLLIN:
+                    request = self.fd_socket[fd].recv(1024).decode("utf-8")
+                    if request:
+                        self.deal_with_request(request, self.fd_socket[fd])
+                    else:
+                        # 在epoll中注销客户端的信息
+                        self.epoll.unregister(fd)
+                        # 关闭客户端的文件句柄
+                        self.fd_socket[fd].close()
+                        # 在字典中删除与已关闭客户端相关的信息
+                        del self.fd_socket[fd]
 
-        new_socket.send(response)
+    def deal_with_request(self, request, client_socket):
+        """为这个浏览器服务器"""
+
+        if not request:
+            return
+
+        request_lines = request.splitlines()
+        for i, line in enumerate(request_lines):
+            print(i, line)
+
+        # 提取请求的文件(index.html)
+        # GET /a/b/c/d/e/index.html HTTP/1.1
+        ret = re.match(r"([^/]*)([^ ]+)", request_lines[0])
+        if ret:
+            print("正则提取数据:", ret.group(1))
+            print("正则提取数据:", ret.group(2))
+            file_name = ret.group(2)
+            if file_name == "/":
+                file_name = "/index.html"
+
+
+        # 读取文件数据
+        try:
+            f = open(self.documents_root+file_name, "rb")
+        except:
+            response_body = "file not found, 请输入正确的url"
+
+            response_header = "HTTP/1.1 404 not found\r\n"
+            response_header += "Content-Type: text/html; charset=utf-8\r\n"
+            response_header += "Content-Length: %d\r\n" % len(response_body)
+            response_header += "\r\n"
+
+            # 将header返回给浏览器
+            client_socket.send(response_header.encode('utf-8'))
+
+            # 将body返回给浏览器
+            client_socket.send(response_body.encode("utf-8"))
+        else:
+            content = f.read()
+            f.close()
+
+            response_body = content
+
+            response_header = "HTTP/1.1 200 OK\r\n"
+            response_header += "Content-Length: %d\r\n" % len(response_body)
+            response_header += "\r\n"
+
+            # 将数据返回给浏览器
+            client_socket.send(response_header.encode("utf-8")+response_body)
+
+
+# 设置服务器服务静态资源时的路径
+DOCUMENTS_ROOT = "./html"
 
 
 def main():
-    """用来完成整体的控制"""
-    # 1. 创建套接字
-    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    """控制web服务器整体"""
+    # python3 xxxx.py 7890
+    if len(sys.argv) == 2:
+        port = sys.argv[1]
+        if port.isdigit():
+            port = int(port)
+    else:
+        print("运行方式如: python3 xxx.py 7890")
+        return
 
-    # 2. 绑定
-    tcp_server_socket.bind(("", 7890))
-
-    # 3. 变为监听套接字
-    tcp_server_socket.listen(128)
-    tcp_server_socket.setblocking(False)  # 将套接字变为非堵塞
-
-    # 创建一个epoll对象
-    epl = select.epoll()
-
-    # 将监听套接字对应的fd注册到epoll中
-    epl.register(tcp_server_socket.fileno(), select.EPOLLIN)
-
-    fd_event_dict = dict()
-
-    while True:
-
-        fd_event_list = epl.poll()  # 默认会堵塞，直到 os监测到数据到来 通过事件通知方式 告诉这个程序，此时才会解堵塞
-
-        # [(fd, event), (套接字对应的文件描述符, 这个文件描述符到底是什么事件 例如 可以调用recv接收等)]
-        for fd, event in fd_event_list:
-            # 等待新客户端的链接
-            if fd == tcp_server_socket.fileno():
-                new_socket, client_addr = tcp_server_socket.accept()
-                epl.register(new_socket.fileno(), select.EPOLLIN)
-                fd_event_dict[new_socket.fileno()] = new_socket
-            elif event == select.EPOLLIN:
-                # 判断已经链接的客户端是否有数据发送过来
-                recv_data = fd_event_dict[fd].recv(1024).decode("utf-8")
-                if recv_data:
-                    service_client(fd_event_dict[fd], recv_data)
-                else:
-                    fd_event_dict[fd].close()
-                    epl.unregister(fd)
-                    del fd_event_dict[fd]
-
-    # 关闭监听套接字
-    tcp_server_socket.close()
+    print("http服务器使用的port:%s" % port)
+    http_server = WSGIServer(port, DOCUMENTS_ROOT)
+    http_server.run_forever()
 
 
 if __name__ == "__main__":
     main()
 
+
+# 小总结
+# I/O 多路复用的特点：
+#
+# 通过一种机制使一个进程能同时等待多个文件描述符，而这些文件描述符（套接字描述符）其中的任意一个进入读就绪状态，epoll()函数就可以返回。 所以, IO多路复用，本质上不会有并发的功能，因为任何时候还是只有一个进程或线程进行工作，它之所以能提高效率是因为select\epoll 把进来的socket放到他们的 '监视' 列表里面，当任何socket有可读可写数据立马处理，那如果select\epoll 手里同时检测着很多socket， 一有动静马上返回给进程处理，总比一个一个socket过来,阻塞等待,处理高效率。
+#
+# 当然也可以多线程/多进程方式，一个连接过来开一个进程/线程处理，这样消耗的内存和进程切换页会耗掉更多的系统资源。 所以我们可以结合IO多路复用和多进程/多线程 来高性能并发，IO复用负责提高接受socket的通知效率，收到请求后，交给进程池/线程池来处理逻辑

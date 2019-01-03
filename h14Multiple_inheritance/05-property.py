34f3f8a10c822687b43c73f13930311b793da279
win10_oulen <henry_oulen@163.com>
2019-01-04 01:28:48 +0800

    property属性

--- h14Multiple_inheritance/05-property.py
+++ h14Multiple_inheritance/05-property.py
@@ -102,7 +102,7 @@ class Goods:
 
 # ############### 调用 ###############
 obj = Goods()
-obj.price          # 自动执行 @property 修饰的 price 方法，并获取方法的返回值
+price = obj.price          # 自动执行 @property 修饰的 price 方法，并获取方法的返回值
 obj.price = 123    # 自动执行 @price.setter 修饰的 price 方法，并将  123 赋值给方法的参数
 del obj.price      # 自动执行 @price.deleter 修饰的 price 方法
 # 注意
